1df631a2cf095a3ed7a586b06e48be407e95badb
Ted Irland <irlandth@gmail.com>
2021-04-28 14:18:50 -0500

    Fixed issue with Root Route

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" })
-})
-
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
